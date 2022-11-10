a4953c02219951de3f9ae4ddfaed89fc701f9c3e
Joshua Wirth <virth@tlmt.ch>
2022-11-10 23:09:40 +0100

    feat: shows from bandsintown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
+import Script from 'next/script';
 import { useState } from 'react';
 import { Grid, SpacingVariants } from '../components/grid';
 import Img from '../components/image';
 import { Link } from '../components/link';
 import { PageHeader } from '../components/page-header';
 import { Section } from '../components/section';
-import { ShowList } from '../components/showlist';
 import { SocialMediaLink } from '../components/social-media-link';
 import Teaser from '../components/teaser';
 import Shows from '../data/shows.json';
@@ -18,12 +18,6 @@ const band = [
 ];
 
 const Home: NextPage = () => {
-  const [showAllShows, setShowAllShows] = useState(false);
-
-  const sortedShows = Shows.sort(({ year: a }, { year: b }) => {
-    return b - a;
-  });
-
   return (
     <div>
       <PageHeader fullscreen={true}>
@@ -40,6 +34,97 @@ const Home: NextPage = () => {
         </Section>
       </PageHeader>
       <main>
+
+        <Section title="Shows" navigationId="shows">
+          <Script charSet="utf-8" src="https://widgetv3.bandsintown.com/main.min.js"></Script>
+          <a
+            className="bit-widget-initializer"
+            data-artist-name="id_15511142"
+            data-background-color="rgba(22,22,22,1)"
+            data-separator-color="#DDDDDD"
+            data-text-color="rgba(255,255,255,1)"
+            data-font="Roboto (mono)"
+            data-auto-style="true"
+            data-button-label-capitalization="UPPERCASE"
+            data-header-capitalization="undefined"
+            data-location-capitalization="capitalize"
+            data-venue-capitalization="capitalize"
+            data-display-local-dates="true"
+            data-local-dates-position="tab"
+            data-display-past-dates="true"
+            data-display-details="false"
+            data-display-lineup=""
+            data-display-start-time=""
+            data-social-share-icon="true"
+            data-display-limit="all"
+            data-date-format="DD.MM.YY (dddd)"
+            data-date-orientation="horizontal"
+            data-date-border-color="#9B9B9B"
+            data-date-border-width="1px"
+            data-date-capitalization="undefined"
+            data-date-border-radius="10px"
+            data-event-ticket-cta-size="medium"
+            data-event-custom-ticket-text="undefined"
+            data-event-ticket-text="TICKETS"
+            data-event-ticket-icon=""
+            data-event-ticket-cta-text-color="#FFFFFF"
+            data-event-ticket-cta-bg-color="#9B9B9B"
+            data-event-ticket-cta-border-color="#9B9B9B"
+            data-event-ticket-cta-border-width="0px"
+            data-event-ticket-cta-border-radius="4px"
+            data-sold-out-button-text-color="#FFFFFF"
+            data-sold-out-button-background-color="#9B9B9B"
+            data-sold-out-button-border-color="#9B9B9B"
+            data-sold-out-button-clickable="true"
+            data-event-rsvp-position="hidden"
+            data-event-rsvp-cta-size="medium"
+            data-event-rsvp-only-show-icon="undefined"
+            data-event-rsvp-text="Erinnere mich"
+            data-event-rsvp-icon=""
+            data-event-rsvp-cta-text-color="#9B9B9B"
+            data-event-rsvp-cta-bg-color="#FFFFFF"
+            data-event-rsvp-cta-border-color="#9B9B9B"
+            data-event-rsvp-cta-border-width="1px"
+            data-event-rsvp-cta-border-radius="4px"
+            data-follow-section-position="top"
+            data-follow-section-alignment="center"
+            data-follow-section-header-text="So verpasst du keine Show:"
+            data-follow-section-cta-size="large"
+            data-follow-section-cta-text="Folge uns bei bandsintown"
+            data-follow-section-cta-icon="true"
+            data-follow-section-cta-text-color="#FFFFFF"
+            data-follow-section-cta-bg-color="rgba(209,166,92,1)"
+            data-follow-section-cta-border-color="rgba(209,166,92,1)"
+            data-follow-section-cta-border-width="0px"
+            data-follow-section-cta-border-radius="4px"
+            data-play-my-city-position="hidden"
+            data-play-my-city-alignment="Center"
+            data-play-my-city-header-text="Keine Show in deiner Nähe?"
+            data-play-my-city-cta-size="medium"
+            data-play-my-city-cta-text="Wir kommen auch zu dir!"
+            data-play-my-city-cta-icon="false"
+            data-play-my-city-cta-text-color="#FFFFFF"
+            data-play-my-city-cta-bg-color="rgba(209,166,92,1)"
+            data-play-my-city-cta-border-color="#9B9B9B"
+            data-play-my-city-cta-border-width="0px"
+            data-play-my-city-cta-border-radius="4px"
+            data-language="de"
+            data-layout-breakpoint="900"
+            data-app-id=""
+            data-affil-code=""
+            data-bit-logo-position="bottomCenter"
+            data-bit-logo-color="rgba(255,255,255,1)"
+            data-optin-font=""
+            data-optin-text-color=""
+            data-optin-bg-color=""
+            data-optin-cta-text-color=""
+            data-optin-cta-bg-color=""
+            data-optin-cta-border-width=""
+            data-optin-cta-border-radius=""
+            data-optin-cta-border-color=""
+          ></a>
+        </Section>
+
         <Section bgColor={true} title="News">
           <Link href="/newsletter" className="mr-8 mb-8">
             Newsletter abbonieren
@@ -60,30 +145,6 @@ const Home: NextPage = () => {
               text="Unsere erste Single im 2022. Zum ersten Mal übernimmt Dominik die Lead Vocals."
               linkText="Song anhören"
             />
-          </Grid>
-        </Section>
-
-        <Section title="Shows" navigationId="shows">
-          <Grid cols="2-lg">
-            {sortedShows
-              .filter(({ year }) => year > 2019)
-              .map(({ year, shows }) => (
-                <ShowList key={year} year={year} shows={shows} />
-              ))}
-          </Grid>
-          <button
-            className="border-b-4 hover:border-sundance mb-4"
-            onClick={() => {
-              setShowAllShows(!showAllShows);
-            }}
-          >
-            {showAllShows ? 'Alte Shows ausblenden' : 'Alle Shows anzeigen'}
-          </button>
-          <Grid cols="2-lg">
-            {showAllShows &&
-              sortedShows
-                .filter(({ year }) => year <= 2019)
-                .map(({ year, shows }) => <ShowList key={year} year={year} shows={shows} />)}
           </Grid>
         </Section>
 
