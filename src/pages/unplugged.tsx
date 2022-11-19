553ba8ac1d4f1bc2a955a318655772740304b3ae
Joshua Wirth <virth@tlmt.ch>
2022-11-19 13:50:32 +0100

    add content

--- src/pages/unplugged.tsx
+++ src/pages/unplugged.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import greenwood from '../../public/img/live/greenwood.png';
+import unplugged from '../../public/img/live/tlmt_unplugged.png';
 import { Button } from '../components/button';
 import { PageHeader } from '../components/page-header';
 import { Section } from '../components/section';
@@ -9,90 +9,83 @@ import { TextBlock } from '../components/text-block';
 import { Heading1 } from '../identity/heading-1';
 import { Heading2 } from '../identity/heading-2';
 import { Heading3 } from '../identity/heading-3';
-import { FacebookIcon, InstagramIcon, SpotifyIcon, YouTubeIcon } from '../identity/icons';
 
 const Unplugged: NextPage = () => {
   return (
     <div>
       <PageHeader title="Unplugged" description="Akkustische Coversongs für deinen nächsten Anlasss.">
-        <Heading1>Hochzeit, Firmenfeier, Events</Heading1>
-        <Image src={greenwood} layout="responsive" className="rounded" alt="Guitar players on stage" />
+        <Heading1>Akustische Coversongs für deinen Event</Heading1>
+        <Image src={unplugged} layout="responsive" className="rounded" alt="Guitar players on stage" />
       </PageHeader>
       <main>
         <Section>
           <div className="grid grid-cols1 lg:grid-cols-[1fr,2fr] gap-8 lg:gap-16">
             <div>
-              <Heading2>Downloads</Heading2>
-              <Link href="/downloads/TLMT-Presskit-Oct-2022.zip">
-                <a className="block my-8">
-                  <Button>Download Presskit</Button>
-                </a>
-              </Link>
-              <Link href="/downloads/TLMT-Presskit/TLMT-Rider_October-2022.pdf">
-                <a className="block my-8">
-                  <Button>Download Tech Rider</Button>
-                </a>
-              </Link>
-              <Heading2 className="mt-16">Musik</Heading2>
+              <Heading2>Musik</Heading2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
                 <iframe
-                  className="w-full h-60 rounded border-sundance border-2"
-                  src="https://www.youtube.com/embed/H2fqc_j0dI4"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
+                  width="100%"
+                  height="300"
+                  scrolling="no"
+                  frameBorder="no"
+                  allow="autoplay"
+                  className="w-full h-40 rounded border-sundance border-2"
+                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/206249134&color=%23D1A65C&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"
                 ></iframe>
                 <iframe
-                  className="w-full h-60 rounded border-sundance border-2"
-                  src="https://www.youtube.com/embed/75pBElJ-w3I"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
+                  width="100%"
+                  height="300"
+                  scrolling="no"
+                  frameBorder="no"
+                  allow="autoplay"
+                  className="w-full h-40 rounded border-sundance border-2"
+                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/206249126&color=%23D1A65C&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"
                 ></iframe>
                 <iframe
-                  className="w-full h-80 rounded border-sundance border-2"
-                  src="https://open.spotify.com/embed/artist/3OdzQZQLrZhJSmNXwXaVed?utm_source=generator"
-                  frameBorder="0"
-                  allowFullScreen
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  width="100%"
+                  height="300"
+                  scrolling="no"
+                  frameBorder="no"
+                  allow="autoplay"
+                  className="w-full h-40 rounded border-sundance border-2"
+                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/206249121&color=%23D1A65C&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"
+                ></iframe>
+                <iframe
+                  width="100%"
+                  height="300"
+                  scrolling="no"
+                  frameBorder="no"
+                  allow="autoplay"
+                  className="w-full h-40 rounded border-sundance border-2"
+                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/206249120&color=%23D1A65C&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"
                 ></iframe>
               </div>
             </div>
             <div>
-              {/* <Heading2>HANDMADE SWISS ROCKMUSIC</Heading2> */}
-
+              <Heading2>Handgemacht</Heading2>
               <TextBlock>
                 Ein unplugged Konzert passt überall. Akustische Gitarren, Bass und Cajon. Wenig Platz, wenig Lärm, super
-                Show.{' '}
+                Show. Passend für deine Hochzeit, für deine Firmenfeier, für deinen Event.{' '}
               </TextBlock>
+              <TextBlock>
+                Wir bringen mit, was du brauchst und sorgen für guten Sound. Musikanlage, Lichtanlage, wenn du willst auch
+                eine kleine Bühne. Du kümmerst dich um deine Gäste oder geniesst deinen Event. Ab CHF 900.- gehts los.
+              </TextBlock>
+              <Link href="/booking">
+                <a className="block my-8">
+                  <Button>Jetzt unverbindlich anfragen</Button>
+                </a>
+              </Link>
 
-              <Heading3>Coversongs von:</Heading3>
+              <Heading3 className="mt-24">Ein Auszug aus unserem Songportfolio:</Heading3>
               <ul className="text-sm md:text-base xl:text-lg">
                 <li>Thinking out loud – Ed Sheeran</li>
-                <li>Coldplay</li>
+                <li>Viva la Vida – Coldplay</li>
                 <li>Learn to Fly – Foo Fighters</li>
                 <li>Opposites – Biffy Clyro</li>
                 <li>Bed of Roses – Bon Jovi</li>
-                <li>und viele mehr</li>
-              </ul>
-              <InstagramIcon size="48" />
-              <Heading3 className="mt-8 lg:mt-16">Social Media Links</Heading3>
-              <ul className="text-sm md:text-base fill-white">
-                <li>
-                  <InstagramIcon size="24" className="inline" /> Instagram: https://instagram.com/timt_rockband
-                </li>
-                <li>
-                  <YouTubeIcon size="24" className="inline" /> YouTube: https://youtube.com/@tlmt_rockband
-                </li>
-                <li>
-                  <FacebookIcon size="24" className="inline" /> Facebook: https://www.facebook.com/tlmtRockband
-                </li>
-                <li>
-                  <SpotifyIcon size="24" className="inline" /> Spotify:
-                  https://open.spotify.com/artist/3OdzQZQLrZhJSmNXwXaVed?si=skgIxaiITc-IYP5mYFDH4A
-                </li>
+                <li>500 Miles – The Proclaimers</li>
+                <li>und viele mehr...</li>
               </ul>
             </div>
           </div>
